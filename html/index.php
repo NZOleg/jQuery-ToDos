9093b24e9fe0261772a151f678976aeaaad3ee33
olegok <NZOlegok@gmail.com>
2018-01-01 17:09:21 +1300

    Initial commit

---
+++ html/index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <title>Document</title>
+    <script
+        src="https://code.jquery.com/jquery-3.2.1.min.js"
+        integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+        crossorigin="anonymous"></script>
+</head>
+<body>
+
+
+<div id="container">
+    <h1>To-Do List <i class="fa fa-plus"></i></h1>
+    <input type="text"placeholder="Add New Todo">
+    <ul>
+        <li><span><i class="fa fa-trash"></i></span>Go To Potions Class</li>
+        <li><span><i class="fa fa-trash"></i></span>Buy New Robes</li>
+        <li><span><i class="fa fa-trash"></i></span>Bisit Hagrid</li>
+    </ul>
+</div>
+
+
+<script src="assets/js/todos.js"></script>
+</body>
+</html>
